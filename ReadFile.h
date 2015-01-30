a362e9d4f4069dd5164d81c696e412b832ccdd5d
Maverick Peppers <bmpeppers@gmail.com>
2015-01-30 14:58:11 -0600

    Update ReadFile.h

--- ReadFile.h
+++ ReadFile.h
@@ -10,6 +10,7 @@ class ReadFile // Switched to a class
 { 
     private: // Made private
     ifstream input_file;
+    
     bool _eof;
     bool closed;
 
@@ -19,12 +20,11 @@ class ReadFile // Switched to a class
 	//deconstructor
 	~ReadFile ();
 	
-	ReadFile* createReadFile(const char* file_name);
-	void destroyReadFile(ReadFile* rf);
-	String* readLine(ReadFile* rf);
-	bool eof(ReadFile* rf);
-	void close(ReadFile* rf);
-	ReadFile();  // This is the constructor
+	void createReadFile(const char* file_name);
+	void destroyReadFile();
+	String* readLine();
+	bool eof();
+	void close();
 };
 
 #endif
